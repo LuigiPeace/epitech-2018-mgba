d0edaaea41ea3084e6762b9c1b6cf96a8e11a180
poirie_l <poirie_l@epitech.eu>
2013-11-25 23:27:16 +0100

    add Makefile. Update gba_loop to handle held's keys.

--- source/gba_loop.c
+++ source/gba_loop.c
@@ -5,7 +5,7 @@
 ** Login   <poirie_l@epitech.net>
 ** 
 ** Started on  Sat Nov 23 22:51:59 2013 Louis Poirier
-** Last update Mon Nov 25 22:38:22 2013 Louis Poirier
+** Last update Mon Nov 25 23:26:01 2013 Louis Poirier
 */
 
 #include	"mgba.h"
@@ -23,6 +23,8 @@ u8		gba_loop(const t_gba *gba)
       scanKeys();
       /* pressed = keysDown(); */
       released = keysUp();
+      if (gba->key_repeat == 1)
+        released = keysHeld();
       if (released != 0)
 	gba->key(released, gba->key_param);
     }
