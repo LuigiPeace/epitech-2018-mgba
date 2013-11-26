b5dcfbfa840343a6a56f7f2728d1851933724d9c
poirie_l <poirie_l@epitech.eu>
2013-11-26 08:48:38 +0100

    main ok to test draw_line & arrows

--- source/main.c
+++ source/main.c
@@ -1,36 +1,56 @@
 /*
-** main.c for libmgba in /home/esl/Documents/Perso/C/GameBoyDev/libmgba
+** main.c for libmgba in /home/esl/Documents/Perso/C/GameBoyDev/libmgba/source
 ** 
 ** Made by Louis Poirier
 ** Login   <poirie_l@epitech.net>
 ** 
-** Started on  Tue Nov 26 08:31:13 2013 Louis Poirier
-** Last update Tue Nov 26 08:33:59 2013 Louis Poirier
+** Started on  Tue Nov 26 08:46:10 2013 Louis Poirier
+** Last update Tue Nov 26 08:46:43 2013 Louis Poirier
 */
 
 #include	"mgba.h"
 
-u8		dots_init(t_dot *start, t_dot *stop)
+typedef struct	s_engine
 {
-  if (start == NULL || stop == NULL)
+  t_gba		*gba;
+  t_dot		start;
+  t_dot		stop;
+}		t_engine;
+
+u8		key_hook(const u16 key, void *param)
+{
+  t_engine	*engine;
+
+  if (param == NULL)
     return (1);
-  start->x = 120;
-  start->y = 80;
-  stop->x = 200;
-  stop->y = 80;
+  engine = param;
+  if (key & KEY_UP)
+    engine->stop.y -= 1;
+  if (key & KEY_DOWN)
+    engine->stop.y += 1;
+  if (key & KEY_LEFT)
+    engine->stop.x -= 1;
+  if (key & KEY_RIGHT)
+    engine->stop.x += 1;
+  gba_clear_screen(engine->gba, 0);
+  gba_draw_line(engine->gba, engine->start, engine->stop, RGB16(31, 31, 31));
   return (0);
 }
 
 int		main(void)
 {
   t_gba		*gba;
-  t_dot		start;
-  t_dot		stop;
+  t_engine	engine;
 
   if ((gba = gba_init(MODE_3 | BG2_ON, 1)) == NULL)
     return (1);
-  dots_init(&start, &stop);
-  gba_draw_line(gba, start, stop, RGB16(31, 31, 31));
+  engine.gba = gba;
+  engine.start.x = 120;
+  engine.start.y = 80;
+  engine.stop.x = 200;
+  engine.stop.y = 80;
+  gba_draw_line(gba, engine.start, engine.stop, RGB16(31, 31, 31));
+  gba_key_hook(gba, &key_hook, &engine);
   gba_loop(gba);
   return (0);
 }
